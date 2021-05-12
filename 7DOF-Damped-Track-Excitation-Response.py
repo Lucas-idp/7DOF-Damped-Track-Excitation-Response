91c5b45f0f954f468f1fac0bcae22da449bae1d3
Lucas-idp <lucas.idp@hotmail.com>
2021-05-12 00:05:53 -0300

    Potential and dissipative energy formulation reviewed

--- 7DOF-Damped-Track-Excitation-Response.py
+++ 7DOF-Damped-Track-Excitation-Response.py
@@ -33,27 +33,44 @@ Ix_carro = 47 #valor qualqr
 Iz_carro = 20
 
 ## Constantes Elásticas Estimadas
-k2a = 150*9.81/0.01
-k3a = 150*9.81/0.01
-k4a = 150*9.81/0.01
-k5a = 150*9.81/0.01
-k2b = k2a/8
-k3b = k3a/8
-k4b = 1.5*k2b
-k5b = 1.5*k3b
+k2a = 150*9.81/0.01 # K amortecedor
+k3a = 150*9.81/0.01 # K amortecedor
+k4a = 150*9.81/0.01 # K amortecedor
+k5a = 150*9.81/0.01 # K amortecedor
+k2b = k2a/8 # K pneu
+k3b = k3a/8 # K pneu
+k4b = 1.5*k2b # K pneu
+k5b = 1.5*k3b # K pneu
 
 ## Constantes de Amortecimento (chutes)
 c1 = 0
 c2 = 200
+c2b = c2/5
 c3 = 200
+c3b = c3/5
 c4 = 200
+c4b = c4/5
 c5 = 200
+c5b = c5/5
 c6 = 0
 c7 = 0
 c_ = [c1, c2, c3, c4, c5, c6, c7]
+
+## Amplitudes e velocidade da excitação de pista
+
+u2 = 0.01
+u3 = 0.01
+u4 = 0.01
+u5 = 0.01
+
+u2_dot = 10
+u3_dot = 10
+u4_dot = 10
+u5_dot = 10
+
 #### Declarando variaveis simbólicas
 
-y1, y2, y3, y4, y5, gama, teta, y1_dot, y2_dot, y3_dot, y4_dot, y5_dot, gama_dot, teta_dot, y1_dot2, y2_dot2, y3_dot2, y4_dot2, y5_dot2, gama_dot2, teta_dot2, t_ = sympy.symbols('y1, y2, y3, y4, y5, gama, teta, y1_dot, y2_dot, y3_dot, y4_dot, y5_dot, gama_dot, teta_dot, y1_dot2, y2_dot2, y3_dot2, y4_dot2, y5_dot2, gama_dot2, teta_dot2, t_')      
+y1, y2, y3, y4, y5, gama, teta, y1_dot, y2_dot, y3_dot, y4_dot, y5_dot, gama_dot, teta_dot, y1_dot2, y2_dot2, y3_dot2, y4_dot2, y5_dot2, gama_dot2, teta_dot2, t_,  = sympy.symbols('y1, y2, y3, y4, y5, gama, teta, y1_dot, y2_dot, y3_dot, y4_dot, y5_dot, gama_dot, teta_dot, y1_dot2, y2_dot2, y3_dot2, y4_dot2, y5_dot2, gama_dot2, teta_dot2, t_')      
 var_dot2 = [y2_dot2, y3_dot2, y4_dot2, y5_dot2, y1_dot2, gama_dot2, teta_dot2] #Variável de "velocidade" para derivada da energia cinética
 var = [y2, y3, y4, y5, y1, gama, teta] #Variável de deslocamento para derivada da energia potencial
 var_dot = [y2_dot, y3_dot, y4_dot, y5_dot, y1_dot, gama_dot, teta_dot] #Variável de velocidade pra derivada da energia dissipada em amortecimento viscoso
@@ -61,8 +78,8 @@ var_dot = [y2_dot, y3_dot, y4_dot, y5_dot, y1_dot, gama_dot, teta_dot] #Variáve
 #### energia cinetica, potencial e dissipativa
 
 Ec = m_carro*(y1_dot2**2)/2 + Ix_carro*(teta_dot2**2)/2 + Iz_carro*(gama_dot2**2)/2 + m_roda*((y2_dot2**2)/2 + (y3_dot2**2)/2 + (y4_dot2**2)/2 + (y5_dot2**2)/2) 
-Ep = ((y1 + (gama*L1) + teta*(L3/2) - y3)**2)*k3b/2 + ((y1 - (gama*L2) + teta*(L3/2)- y2)**2)*k2b/2  + ((y1 + (gama*L2) - teta*(L3/2) - y4)**2)*k4b/2 + ((y1 - (gama*L2) - teta*(L3/2) - y5)**2)*k5b/2  + (y2**2)*k2a/2 + (y3**2)*k3a/2 + (y4**2)*k4a/2 + (y5**2)*k5a/2 + m_roda*g*(y1 + y2 + y3 + y4 + y5)
-Ed = c2*(1/2)*(y1_dot - y2_dot - teta_dot*L3/2 + gama_dot*L1)**2 + c3*(1/2)*(y1_dot - y3_dot + teta_dot*L3/2 + gama_dot*L1)**2 + c4*(1/2)*(y1_dot - y4_dot + teta_dot*L3/2 - gama_dot*L2)**2 + c5*(1/2)*(y1_dot - y5_dot - teta_dot*L3/2 - gama_dot*L2)**2
+Ep = ((y1 + (gama*L1) + teta*(L3/2) - y3)**2)*k3a/2 + ((y1 - (gama*L2) + teta*(L3/2)- y2)**2)*k2a/2  + ((y1 + (gama*L2) - teta*(L3/2) - y4 + u4)**2)*k4a/2 + ((y1 - (gama*L2) - teta*(L3/2) - y5)**2)*k5a/2  + ((y2 - u2)**2)*k2b/2 + ((y3 - u3)**2)*k3b/2 + ((y4 - u4)**2)*k4b/2 + ((y5 - u5)**2)*k5b/2 
+Ed = c2*(1/2)*(y1_dot - y2_dot - teta_dot*L3/2 + gama_dot*L1)**2 + c3*(1/2)*(y1_dot - y3_dot + teta_dot*L3/2 + gama_dot*L1)**2 + c4*(1/2)*(y1_dot - y4_dot + teta_dot*L3/2 - gama_dot*L2)**2 + c5*(1/2)*(y1_dot - y5_dot - teta_dot*L3/2 - gama_dot*L2)**2 + c2b*(1/2)*(y2_dot**2 - u2_dot**2) + c3b*(1/2)*(y3_dot**2 - u3_dot**2) + c4b*(1/2)*(y4_dot**2 - u4_dot**2) + c5b*(1/2)*(y5_dot**2 - u5_dot**2)
 
 ## Força dissipativa
 #Q = [- c2*y2_dot, - c3*y3_dot, - c4*y4_dot, - c5*y5_dot, - c1*y1_dot, - c6*gama_dot, - c7*teta_dot]
@@ -237,26 +254,35 @@ print('A matriz meff é simétrica')
 
 
 #### Condições iniciais
-x0 = [0,0,0,0,0,0,0]
-v0 = [0.9,0.9,0,0,0,0,0]
+X = 0.01 #[m]
+wb= 20 #[rad/s]
+r = wb/wn
 
-y0 = np.dot(np.dot(B.T,M),x0) #Condições iniciais em coordenadas modais
-y0dot = np.dot(np.dot(B.T,M),v0)
 
-t_inc0 = 0.05 #segundos
+
+
+
+
+
+
+
+
+#y0 = np.dot(np.dot(B.T,M),x0) #Condições iniciais em coordenadas modais
+#y0dot = np.dot(np.dot(B.T,M),v0)
+
+t_inc0 = 0.005 #segundos
 t_end0 = 2 #segundos    
 t = np.arange(0, t_end0, t_inc0)
 
 y = []
-exp_qsi = []
-for i in range(len(var)): #Pré-calculando primeiro termo da função resposta, "mágica"
-    exp_qsi.append(0)
-    exp_qsi[i] = np.exp(-qsi[i]*freq_n[i]*t)
+phi_x = []
     
-for r in range(len(var)): #Obtendo a resposta no tempo em coordenadas modais para cada grau de liberdade
-    y.append(0)
-    y[r] = exp_qsi[r]*(y0[r]*np.cos(freq_d[r]*t) + ((y0dot[r] + qsi[r]*freq_n[r]*y0[r])/freq_d[r])*np.sin(freq_d[r]*t))
-    
+for i in range(len(var)): #Obtendo a resposta no tempo em coordenadas modais para cada grau de liberdade
+     y.append(0)
+     phi_x.append(0)
+     phi_x[i] = np.arctan((2*qsi[i]*r[i]**3)/(1-(r[i]**2)+(2*qsi[i]*r[i])**2))
+     y[i] = ((X*np.sqrt(1+(2*qsi[i]*r[i])**2))/np.sqrt((1-r[i]**2)**2 + (2*qsi[i]*r[i])**2))*np.sin(wb*t - phi_x[i])
+     
 x = np.dot(B,y) #Convertendo resposta de coordenadas modais para físicas
 x1 = x[0][:]
 x2 = x[1][:]
@@ -270,9 +296,9 @@ for i in range(len(var)):
     
       plt.plot(t,x[i][:])
 
-# plt.plot(t,x6)
-# plt.plot(t,x7)
-np.savetxt('7GDL_RespostaAMORTECIDAteste5', np.transpose([x1,x2,x3,x4,x5,x6,x7]), fmt='%1.5f')
+# # plt.plot(t,x6)
+# # plt.plot(t,x7)
+# np.savetxt('7GDL_RespostaAMORTECIDAteste5', np.transpose([x1,x2,x3,x4,x5,x6,x7]), fmt='%1.5f')
 
 
 
